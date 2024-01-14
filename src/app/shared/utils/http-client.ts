7f14d72714400f4eaaf581c64fe5424710cac284
marcinludwig <3653161+und3fin3d@users.noreply.github.com>
2024-01-14 22:04:59 +0100

    feat: add axios

--- src/app/shared/utils/http-client.ts
+++ src/app/shared/utils/http-client.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import { getItem } from './faker/data.faker';
 
 const wait = (data: unknown) =>
@@ -7,7 +9,13 @@ const wait = (data: unknown) =>
     }, 500);
   });
 
+axios.interceptors.request.use(function (config) {
+  config.url = `http://localhost:3000${config.url}`;
+  return config;
+});
+
 export const getHttpClient = () => {
+  // return axios;
   return {
     get: <T>(url: string) => wait(getItem(url)) as Promise<T>,
     post: <T>(url: string) => wait(getItem(url)) as Promise<T>,
